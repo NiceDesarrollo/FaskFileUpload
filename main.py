38e803bb315aa2b3e40cd13234abd0f5b74b02b4
Gabriel Ahumada <gahumada@nice.com.mx>
2024-02-20 16:04:31 -0600

    easy code to save file

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import os
 app = Flask(__name__)
 
 #Nombre de la carpeta donde se guardaran los documentos
-app.config["UPLOAD_DIR"] = "uploads"
 @app.route("/upload", methods = ["POST"])
 def upload():
-        file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_DIR'], file.filename))
+        f = request.files['file']
+        f.save('./uploads/audio.mp3')
         return "success"
 
 if __name__ == "__main__":
