39cc540476b55db178bbae10d0190cddf462809d
Gabriel Ahumada <gahumada@nice.com.mx>
2024-02-20 13:20:54 -0600

    second commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+app.config["UPLOAD_DIR"] = "uploads"
+@app.route("/upload", methods = ["POST"])
+def upload():
+        file = request.files['file']
+        file.save(os.path.join(app.config['UPLOAD_DIR'], file.filename))
+        return "success"
+
+if __name__ == "__main__":
+    app.run()
